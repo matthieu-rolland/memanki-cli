c8cfb46aa2ba7867533fe2d73a245bdd00e13dfd
mrolland <matthieu.rolland1985@gmail.com>
2017-07-29 12:24:59 +0200

    - add reverse options

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const memanki = require('memanki');
 const argv = require('minimist')(process.argv.slice(2));
 
 const merge = (typeof argv['m'] !== 'undefined' || typeof argv['merge'] !== 'undefined') ? true : false;
+const reverse = (typeof argv['reverse'] !== 'undefined') ? argv['reverse'] : 'none';
 if (typeof argv['id'] === 'undefined' && typeof argv['help'] === 'undefined') {
   console.log('No memrise id was given.')
   process.exit()
 }
-memanki.getAnkiDeck(argv['id'], merge)
+memanki.getAnkiDeck(argv['id'], merge, reverse);
